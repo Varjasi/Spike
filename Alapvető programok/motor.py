074b59be8d69191e2742123e50b63f2f1bea0c57
Varjasi <144667167+Varjasi@users.noreply.github.com>
2024-02-27 15:55:34 +0100

    update 3

--- Alapvető programok/motor.py
+++ Alapvető programok/motor.py
@@ -4,18 +4,18 @@ from pybricks.tools import wait
 
 def indít():
     # Initialize motors on port A and B.
-    track_motor = Motor(Port.A)
-    gripper_motor = Motor(Port.B)
+    Jobb_motor = Motor(Port.A, )
+    Bal_motor = Motor(Port.B)
 
     # Make both motors run at 500 degrees per second.
-    track_motor.run(-500)
-    gripper_motor.run(500)
+    Jobb_motor.run_angle(-360, 360, wait=False)
+    Bal_motor.run_angle(200, 360, then=None)
 
     # Wait for three seconds.
-    wait(3000)
+    wait(1000)
     def call():
         import subprocess
-        subprocess.run("pybricksdev run ble motor Alapvető parancsok\definálása.py")
+        subprocess.run("pybricksdev run ble motor.py")
     call()
 
 
