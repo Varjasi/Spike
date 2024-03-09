f916240debb4656c22df87d34100e8f71eee9ba0
Varjasi <144667167+Varjasi@users.noreply.github.com>
2024-03-09 20:44:32 +0100

    update 3

---
+++ 2024.03.09/1.py
@@ -0,0 +1,34 @@
+import pybricks as file
+from pybricks.pupdevices import Motor
+from pybricks.parameters import Port
+from pybricks.parameters import Stop
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import ColorSensor
+from pybricks.tools import wait
+from pybricks.parameters import Axis
+
+
+szín = ColorSensor(Port.D)
+hub = PrimeHub()
+
+calibrated = hub.imu.ready()
+
+dallam = ["C4/4", "C4/4", "G4/4", "G4/4"]
+hub.speaker.play_notes(dallam, tempo=400)
+
+küszöbérték = hub.imu.settings(1.5, 250)
+print(küszöbérték)
+index = 0
+while index < 10:
+        a = hub.imu.up()
+        calibrated = hub.imu.ready()
+        stationary = hub.imu.stationary()
+        print(stationary)
+        #print(a)
+        küszöbérték = hub.imu.settings()
+        print(küszöbérték)
+        print(hub.imu.heading())
+        print(hub.imu.acceleration())
+        wait(500)
+        index += 1
+
