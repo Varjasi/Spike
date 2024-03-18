492eceb92c015bfdb67c61269e0aa5d1bd8f53d0
Varjasi <144667167+Varjasi@users.noreply.github.com>
2024-03-18 22:36:46 +0100

    update 3

---
+++ 2024.03.18/drivebase.py
@@ -0,0 +1,11 @@
+from pybricks.hubs import InventorHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+from pybricks.tools import multitask, run_task
+
+drive = DriveBase()
+Motor
+
+drive.straight(50, then=Stop.HOLD, wait=False)
