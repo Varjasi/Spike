cdf604e55a558d71541978f99028b1ee1c23e4b5
Varjasi <teodorvarjasi.puskas@gmail.com>
2024-04-25 15:24:04 +0200

    update

--- 2024.03.23/keyboard.py
+++ 2024.03.23/keyboard.py
@@ -7,7 +7,7 @@ import usys
 a = FileIO(open('adatok.txt', '+rb'))
 print(stdin)
 '''
-'''
+
 # Import required MicroPython libraries.
 from usys import stdin
 from uselect import poll
@@ -18,7 +18,7 @@ with FileIO(open('2024.03.23/adatok.txt', 'w', encoding='utf-8')) as a:
     print('Ez kerül a fájlba', file=a)
 
 
-'''
+
 
 with open('2024.03.23/adatok.txt', 'w', encoding='utf-8') as celfajl:
     print('Ez kerül q afájlba', file=celfajl)
